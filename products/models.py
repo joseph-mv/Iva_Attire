bd16b323befe2940c7721cb0ac1bd1fa9ebf98f3
joseph-mv <josephmv43@gmail.com>
2025-03-02 08:41:53 +0530

    feat:add models 4 apps

--- products/models.py
+++ products/models.py
@@ -2,5 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+# Model for product
 
+class Product(models.Model):
+    LIVE=1
+    DELETE=0
+    DELETE_CHOICES=((LIVE,'Live'),(DELETE,'Delete'))
+    title=models.CharField(max_length=250)
+    price=models.FloatField()
+    image=models.ImageField(upload_to='media/products/')
+    description=models.TextField()
+    priority=models.IntegerField(default=0)
+    delete_status=models.IntegerField(choices=DELETE_CHOICES ,default=LIVE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self) ->str:
+        return self.title
 
